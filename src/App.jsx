5cd7d82fa109357c5d7c2c174d860f899776bc03
adityasinghchouhan1 <asadityasingh2572000@gmail.com>
2025-08-12 11:29:43 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import { Outlet } from 'react-router-dom'
 import Navbar from './Pages/Navbar'
 import Footer from './Pages/Footer'
 import ScrollProgress from './UI/ScrollProgress' // adjust path if needed
-
+import ScrollToTop from './ScrollToTop/ScrollToTop'
 const App = () => {
   return (
     <div>
+      <ScrollToTop />
       <Navbar />
       <div className="hidden sm:flex">
         <ScrollProgress />
